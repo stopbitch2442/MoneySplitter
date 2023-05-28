451f9d34cb03646e85977d9750f1c666c39a4d6e
stopbitch2442 <t.khmelevv@yandex.ru>
2023-05-29 00:09:17 +0300

    Add First Migration

---
+++ MoneySplitterApi/Model/MoneySplitterDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MoneySplitterApi.Model
+{
+    public class MoneySplitterDbContext : DbContext
+    {
+        public MoneySplitterDbContext(DbContextOptions<MoneySplitterDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<User>().HasKey(b => b.guid);
+            base.OnModelCreating(builder);
+        }
+
+    }
+}
--- MoneySplitterApi/Program.cs
+++ MoneySplitterApi/Program.cs
@@ -1,22 +1,37 @@
+using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using MoneySplitterApi.Model;
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
 builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddMvc();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "MoneySplitter", Version = "v1" });
+});
+builder.Services.AddDbContext<MoneySplitterDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("SqliteConnection")));
+
 var app = builder.Build();
+
+// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseDeveloperExceptionPage();
-}
-else
-{
-    app.UseExceptionHandler("/Error");
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-app.UseStaticFiles();
-app.UseRouting();
+
 app.UseAuthorization();
 
 app.MapControllers();
+
 app.Run();
